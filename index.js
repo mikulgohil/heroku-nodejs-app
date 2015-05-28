ac328a64bdf751cd40a04069f63fb92e734cc76b
MikulGohil <mikulgohil@gmail.com>
2015-05-28 12:34:18 +0530

    Testing

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World! Testing on Heroku')
+  response.send('Hello World! Testing on Heroku working');
 })
 
 app.listen(app.get('port'), function() {
