76476d1674602d4e7f4215ffd42dc777fb6707d9
Mikul Gohil <mikulgohil@gmail.com>
2015-05-29 08:37:24 +0530

    added jade and bootstrap

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 var express = require('express')
 var app = express();
+app.set('view engine', 'jade');
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
-app.get('/', function(request, response) {
-  response.send('Hello World!');
-})
+app.get('/', function (request, response) {
+  response.render('index', { title: 'Hey', message: 'Hello there!', detail: 'Welcome to My app'});
+});
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
