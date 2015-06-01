85f7aaa3467e6ee6cafbeb22645c9144835d304d
Mikul Gohil <mikulgohil@gmail.com>
2015-06-01 23:12:53 +0530

    heroic error fix for jade

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var express = require('express')
 var app = express();
-app.set('view engine', 'jade');
+app.engine('jade', require('jade').__express);
+//app.set('view engine', 'jade');
+
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
