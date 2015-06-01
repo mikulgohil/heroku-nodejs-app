2cef38bc7b7e72554751f6821698cb175cf58f68
Mikul Gohil <mikulgohil@gmail.com>
2015-06-01 23:07:37 +0530

    route testing

--- index.js
+++ index.js
@@ -6,7 +6,11 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function (request, response) {
-  response.render('index', { title: 'Hey', message: 'Hello there!', detail: 'Welcome to My app'});
+  response.render('index', { title: 'Hey', message: 'Hello there! Home', detail: 'Welcome to My app', link:'about'});
+});
+
+app.get('/about', function (request, response) {   response.render('index', {
+title: 'Hey', message: 'This is about page!!', detail: 'Welcome to My app', link:'/'});
 });
 
 app.listen(app.get('port'), function() {
